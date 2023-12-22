87c05ed12081bc124fb01e756eecd57fb9fa9761
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2023-12-22 17:11:06 -0500

    completed Turkey Hook

--- aa14-react-hooks-useEffect-turkey/src/components/Message.jsx
+++ aa14-react-hooks-useEffect-turkey/src/components/Message.jsx
@@ -1,11 +1,17 @@
-function Message({ size }) {
-  console.log('Message', size);
+import { useEffect, useState } from "react";
 
-  return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
-    </div>
-  );
+function Message({ featherCount, sizeClass }) {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (featherCount <= 0) setMessage("Oh my! Your bird is naked!");
+    else if (featherCount >= 10) {
+      setMessage("Full turkey!");
+    } else {
+      setMessage("Coming along...");
+    }
+  }, [featherCount]);
+  return <div className={`message ${sizeClass}`}>{message}</div>;
 }
 
 export default Message;
